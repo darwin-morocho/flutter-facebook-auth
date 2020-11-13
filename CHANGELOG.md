8d6bf358a430fa3534539a265086ccae0dbbfe66
the-meedu-app <darwin@iMac-de-Darwin.local>
2020-11-13 12:02:37 -0500

    added dartdoc comments

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 1.0.0+2
+- Added dartdoc comments.
+
+
 ## 1.0.0+1
 - Updated documentation.
 
--- lib/src/access_token.dart
+++ lib/src/access_token.dart
@@ -1,11 +1,32 @@
+/// Class that contains the facebook access token data
 class AccessToken {
-  final DateTime expires, lastRefresh;
-  final String userId, token, applicationId, graphDomain;
+  /// DateTime with the expires date of this token
+  final DateTime expires;
 
+  /// DateTime with the last refresh date of this token
+  final DateTime lastRefresh;
+
+  /// the facebook user id
+  final String userId;
+
+  /// token provided by facebook to make api calls to the GRAPH API
+  final String token;
+
+  // the facebook application Id
+  final String applicationId;
+
+  final String graphDomain;
+
+  /// list of string with the rejected permission by the user
   final List<String> declinedPermissions;
+
+  /// list of string with the approved permission by the user
   final List<String> grantedPermissions;
+
+  // is `true` when the token is expired
   final bool isExpired;
 
+  /// constrcutor
   AccessToken({
     this.declinedPermissions,
     this.grantedPermissions,
@@ -18,6 +39,9 @@ class AccessToken {
     this.isExpired,
   });
 
+  /// convert the data provided for the platform channel to one instance of AccessToken
+  ///
+  /// [json] data returned by the platform channel
   factory AccessToken.fromJson(Map<String, dynamic> json) {
     return AccessToken(
       userId: json['userId'],
@@ -27,15 +51,12 @@ class AccessToken {
       applicationId: json['applicationId'],
       graphDomain: json['graphDomain'],
       isExpired: json['isExpired'],
-      declinedPermissions: json['declinedPermissions'] != null
-          ? List<String>.from(json['declinedPermissions'])
-          : [],
-      grantedPermissions: json['grantedPermissions'] != null
-          ? List<String>.from(json['grantedPermissions'])
-          : [],
+      declinedPermissions: json['declinedPermissions'] != null ? List<String>.from(json['declinedPermissions']) : [],
+      grantedPermissions: json['grantedPermissions'] != null ? List<String>.from(json['grantedPermissions']) : [],
     );
   }
 
+  /// convert this instance to one Map
   Map<String, dynamic> toJson() => {
         'userId': this.userId,
         'token': this.token,
