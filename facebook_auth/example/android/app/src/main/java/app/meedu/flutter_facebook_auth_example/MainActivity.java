c479403da44c24674b439728c0c444200e0ef1cc
Darwin Morocho <darwin.morocho@icloud.com>
2021-08-24 10:23:35 -0500

    updated example

--- facebook_auth/example/android/app/src/main/java/app/meedu/flutter_facebook_auth_example/MainActivity.java
+++ facebook_auth/example/android/app/src/main/java/app/meedu/flutter_facebook_auth_example/MainActivity.java
@@ -1,6 +1,18 @@
 package app.meedu.flutter_facebook_auth_example;
 
-import io.flutter.embedding.android.FlutterActivity;
+import android.os.Bundle;
 
-public class MainActivity extends FlutterActivity {
+import androidx.annotation.NonNull;
+
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.android.FlutterFragmentActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterFragmentActivity {
+
+    @Override
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
 }
