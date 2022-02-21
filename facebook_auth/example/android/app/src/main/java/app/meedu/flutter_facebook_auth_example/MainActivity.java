95263a4855fe89716bc0dad400cdf8012e27d270
Darwin <darwin.morocho@icloud.com>
2022-02-21 08:04:14 -0500

    updated facebook sdk to 12.3.0 on Android

--- facebook_auth/example/android/app/src/main/java/app/meedu/flutter_facebook_auth_example/MainActivity.java
+++ facebook_auth/example/android/app/src/main/java/app/meedu/flutter_facebook_auth_example/MainActivity.java
@@ -9,10 +9,6 @@ import io.flutter.embedding.android.FlutterFragmentActivity;
 import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
-public class MainActivity extends FlutterFragmentActivity {
+public class MainActivity extends FlutterActivity {
 
-    @Override
-    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
-        GeneratedPluginRegistrant.registerWith(flutterEngine);
-    }
 }
