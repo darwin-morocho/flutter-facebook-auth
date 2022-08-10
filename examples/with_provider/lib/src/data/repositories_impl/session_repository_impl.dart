68b56e7c04d99f30497cde055be788eaed70a4e1
darwin-morocho <darwin.morocho@icloud.com>
2022-08-10 09:15:36 -0500

    updated examples

---
+++ examples/with_provider/lib/src/data/repositories_impl/session_repository_impl.dart
@@ -0,0 +1,31 @@
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+import 'package:flutter_facebook_auth_example/src/domain/models/user.dart';
+import 'package:flutter_facebook_auth_example/src/domain/repositories/session_repository.dart';
+
+class SessionRepositoryImpl implements SessionRepository {
+  final FacebookAuth _auth;
+
+  SessionRepositoryImpl(this._auth);
+  @override
+  Future<LoginResult> logIn() {
+    return _auth.login();
+  }
+
+  @override
+  Future<User?> get user async {
+    if (await _auth.accessToken != null) {
+      final userData = await _auth.getUserData();
+
+      if (userData.isNotEmpty) {
+        final user = User(
+          userId: userData['id'],
+          name: userData['name'],
+          email: userData['email'],
+          profilePicture: userData['picture']?['data']?['url'],
+        );
+        return user;
+      }
+    }
+    return null;
+  }
+}
--- examples/with_provider/lib/src/ui/routes/app_routes.dart
+++ examples/with_provider/lib/src/ui/routes/app_routes.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_facebook_auth_example/src/ui/pages/splash/views/splash_page.dart';
 import 'package:flutter_facebook_auth_example/src/ui/routes/routes.dart';
 
 import '../pages/home/views/home_page.dart';
@@ -7,7 +6,6 @@ import '../pages/login/views/login_page.dart';
 
 Map<String, Widget Function(BuildContext)> get appRoutes {
   return {
-    Routes.splash: (_) => const SplashPage(),
     Routes.login: (_) => const LoginPage(),
     Routes.home: (_) => const HomePage(),
   };
