4e6714efbad4a67f435da878f781b478e1161374
Darwin <darwin.morocho@icloud.com>
2023-02-24 13:59:07 -0500

    updated example

--- facebook_auth/example/android/app/src/main/kotlin/app/meedu/facebook_auth_example/MainActivity.kt
+++ facebook_auth/example/android/app/src/main/kotlin/app/meedu/facebook_auth_example/MainActivity.kt
@@ -1,6 +1,6 @@
 package app.meedu.facebook_auth_example
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
 }
