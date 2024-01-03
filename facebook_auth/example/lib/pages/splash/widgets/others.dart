4ac979ab97f34c879e4607ef35ca6aaa709e4945
kwaw kumi <kwawkumi9@gmail.com>
2024-01-03 22:41:42 +0000

    changes

--- facebook_auth/example/lib/pages/splash/widgets/others.dart
+++ facebook_auth/example/lib/pages/splash/widgets/others.dart
@@ -1,81 +1,81 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_bloc_social_login/flutter_bloc_social_login.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'package:flutter_bloc_social_login/flutter_bloc_social_login.dart';
 
 
-class SocialMediaConstants {
-  static final facebookConfig = SocialConfig.facebook(
-    appId: 'YOUR_FACEBOOK_APP_ID',
-    appSecret: 'YOUR_FACEBOOK_APP_SECRET',
-    redirectUri: 'YOUR_FACEBOOK_REDIRECT_URI',
-  );
+// class SocialMediaConstants {
+//   static final facebookConfig = SocialConfig.facebook(
+//     appId: 'YOUR_FACEBOOK_APP_ID',
+//     appSecret: 'YOUR_FACEBOOK_APP_SECRET',
+//     redirectUri: 'YOUR_FACEBOOK_REDIRECT_URI',
+//   );
 
-  static final twitterConfig = SocialConfig.twitter(
-    consumerKey: 'YOUR_TWITTER_CONSUMER_KEY',
-    consumerSecret: 'YOUR_TWITTER_CONSUMER_SECRET',
-    redirectUri: 'YOUR_TWITTER_REDIRECT_URI',
-  );
+//   static final twitterConfig = SocialConfig.twitter(
+//     consumerKey: 'YOUR_TWITTER_CONSUMER_KEY',
+//     consumerSecret: 'YOUR_TWITTER_CONSUMER_SECRET',
+//     redirectUri: 'YOUR_TWITTER_REDIRECT_URI',
+//   );
 
-  static final googleConfig = SocialConfig.google(
-    clientId: 'YOUR_GOOGLE_CLIENT_ID',
-    clientSecret: 'YOUR_GOOGLE_CLIENT_SECRET',
-    redirectUri: 'YOUR_GOOGLE_REDIRECT_URI',
-  );
+//   static final googleConfig = SocialConfig.google(
+//     clientId: 'YOUR_GOOGLE_CLIENT_ID',
+//     clientSecret: 'YOUR_GOOGLE_CLIENT_SECRET',
+//     redirectUri: 'YOUR_GOOGLE_REDIRECT_URI',
+//   );
 
-  static final instagramConfig = SocialConfig.instagram(
-    clientId: 'YOUR_INSTAGRAM_CLIENT_ID',
-    clientSecret: 'YOUR_INSTAGRAM_CLIENT_SECRET',
-    redirectUri: 'YOUR_INSTAGRAM_REDIRECT_URI',
-  );
-}
+//   static final instagramConfig = SocialConfig.instagram(
+//     clientId: 'YOUR_INSTAGRAM_CLIENT_ID',
+//     clientSecret: 'YOUR_INSTAGRAM_CLIENT_SECRET',
+//     redirectUri: 'YOUR_INSTAGRAM_REDIRECT_URI',
+//   );
+// }
 
-class SocialMediaConstants {
-  static final facebookConfig = SocialConfig.facebook(
-    appId: 'YOUR_FACEBOOK_APP_ID',
-    appSecret: 'YOUR_FACEBOOK_APP_SECRET',
-    redirectUri: 'YOUR_FACEBOOK_REDIRECT_URI',
-  );
+// class SocialMediaConstants {
+//   static final facebookConfig = SocialConfig.facebook(
+//     appId: 'YOUR_FACEBOOK_APP_ID',
+//     appSecret: 'YOUR_FACEBOOK_APP_SECRET',
+//     redirectUri: 'YOUR_FACEBOOK_REDIRECT_URI',
+//   );
 
-  static final twitterConfig = SocialConfig.twitter(
-    consumerKey: 'YOUR_TWITTER_CONSUMER_KEY',
-    consumerSecret: 'YOUR_TWITTER_CONSUMER_SECRET',
-    redirectUri: 'YOUR_TWITTER_REDIRECT_URI',
-  );
+//   static final twitterConfig = SocialConfig.twitter(
+//     consumerKey: 'YOUR_TWITTER_CONSUMER_KEY',
+//     consumerSecret: 'YOUR_TWITTER_CONSUMER_SECRET',
+//     redirectUri: 'YOUR_TWITTER_REDIRECT_URI',
+//   );
 
-  static final googleConfig = SocialConfig.google(
-    clientId: 'YOUR_GOOGLE_CLIENT_ID',
-    clientSecret: 'YOUR_GOOGLE_CLIENT_SECRET',
-    redirectUri: 'YOUR_GOOGLE_REDIRECT_URI',
-  );
+//   static final googleConfig = SocialConfig.google(
+//     clientId: 'YOUR_GOOGLE_CLIENT_ID',
+//     clientSecret: 'YOUR_GOOGLE_CLIENT_SECRET',
+//     redirectUri: 'YOUR_GOOGLE_REDIRECT_URI',
+//   );
 
-  static final instagramConfig = SocialConfig.instagram(
-    clientId: 'YOUR_INSTAGRAM_CLIENT_ID',
-    clientSecret: 'YOUR_INSTAGRAM_CLIENT_SECRET',
-    redirectUri: 'YOUR_INSTAGRAM_REDIRECT_URI',
-  );
-}
+//   static final instagramConfig = SocialConfig.instagram(
+//     clientId: 'YOUR_INSTAGRAM_CLIENT_ID',
+//     clientSecret: 'YOUR_INSTAGRAM_CLIENT_SECRET',
+//     redirectUri: 'YOUR_INSTAGRAM_REDIRECT_URI',
+//   );
+// }
 
-class SocialMediaConstants {
-  static final facebookConfig = SocialConfig.facebook(
-    appId: 'YOUR_FACEBOOK_APP_ID',
-    appSecret: 'YOUR_FACEBOOK_APP_SECRET',
-    redirectUri: 'YOUR_FACEBOOK_REDIRECT_URI',
-  );
+// class SocialMediaConstants {
+//   static final facebookConfig = SocialConfig.facebook(
+//     appId: 'YOUR_FACEBOOK_APP_ID',
+//     appSecret: 'YOUR_FACEBOOK_APP_SECRET',
+//     redirectUri: 'YOUR_FACEBOOK_REDIRECT_URI',
+//   );
 
-  static final twitterConfig = SocialConfig.twitter(
-    consumerKey: 'YOUR_TWITTER_CONSUMER_KEY',
-    consumerSecret: 'YOUR_TWITTER_CONSUMER_SECRET',
-    redirectUri: 'YOUR_TWITTER_REDIRECT_URI',
-  );
+//   static final twitterConfig = SocialConfig.twitter(
+//     consumerKey: 'YOUR_TWITTER_CONSUMER_KEY',
+//     consumerSecret: 'YOUR_TWITTER_CONSUMER_SECRET',
+//     redirectUri: 'YOUR_TWITTER_REDIRECT_URI',
+//   );
 
-  static final googleConfig = SocialConfig.google(
-    clientId: 'YOUR_GOOGLE_CLIENT_ID',
-    clientSecret: 'YOUR_GOOGLE_CLIENT_SECRET',
-    redirectUri: 'YOUR_GOOGLE_REDIRECT_URI',
-  );
+//   static final googleConfig = SocialConfig.google(
+//     clientId: 'YOUR_GOOGLE_CLIENT_ID',
+//     clientSecret: 'YOUR_GOOGLE_CLIENT_SECRET',
+//     redirectUri: 'YOUR_GOOGLE_REDIRECT_URI',
+//   );
 
-  static final instagramConfig = SocialConfig.instagram(
-    clientId: 'YOUR_INSTAGRAM_CLIENT_ID',
-    clientSecret: 'YOUR_INSTAGRAM_CLIENT_SECRET',
-    redirectUri: 'YOUR_INSTAGRAM_REDIRECT_URI',
-  );
-}
+//   static final instagramConfig = SocialConfig.instagram(
+//     clientId: 'YOUR_INSTAGRAM_CLIENT_ID',
+//     clientSecret: 'YOUR_INSTAGRAM_CLIENT_SECRET',
+//     redirectUri: 'YOUR_INSTAGRAM_REDIRECT_URI',
+//   );
+// }
